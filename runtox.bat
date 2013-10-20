b9fc957218e1a3b16550417180db2a186e2ad035
Christian Heimes <christian@cheimes.de>
2013-10-20 13:00:48 +0200

    Also test Python 2.4 and 2.5

--- runtox.bat
+++ runtox.bat
@@ -1,2 +1,15 @@
-@"c:\Program Files\Python27\Scripts\tox.exe"
+@echo off
+cls
+"c:\Program Files\Python27\Scripts\tox.exe"
+
+echo.
+echo --------------
+echo Python 2.4 X86
+"c:\Program Files (x86)\Python24\python.exe" tests.py
+
+echo.
+echo --------------
+echo Python 2.5 X86
+"c:\Program Files (x86)\Python25\python.exe" tests.py
+
 pause
